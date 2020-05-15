c31b668855d8a3f5264b91f006caf487f8df2210
tfmcmahon <tfmcmahon@gmail.com>
2020-05-15 09:42:15 -0500

    Updates

---
+++ precache-manifest.605cadea29ac75e252ed729888687a36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ad82f08ef00a14f9b0534cd03bbefd",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "688c14120f547a5fd49c",
+    "url": "/tetris/static/css/main.d36246a9.chunk.css"
+  },
+  {
+    "revision": "20aee53a1ac19d568193",
+    "url": "/tetris/static/js/2.8bd73246.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tetris/static/js/2.8bd73246.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "688c14120f547a5fd49c",
+    "url": "/tetris/static/js/main.68096028.chunk.js"
+  },
+  {
+    "revision": "d277841e83d8346ade54",
+    "url": "/tetris/static/js/runtime-main.1e5dbec4.js"
+  }
+]);
